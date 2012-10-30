b9a4c5343b6af1f5687d82d45756bef11ef628cd
martindavis <martin@md3productions.com>
2012-10-30 00:37:47 -0400

    Initial commit with the library included.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ trigger-backbone.sync
 =====================
 
 Trigger.io Backbone.Sync
+
+This library was created so that Trigger.io users can simply plug it in and start using Backbone's Models and Collections. Enjoy and use at your own risk!
---
+++ backbone.sync.trigger.js
@@ -0,0 +1,95 @@
+/*
+ * Define custom Backbone.sync to use forge.request.ajax
+ *
+ * Look for Backbone.ajax in the next release so that we don't have to overwrite
+ * the entire Backbone.sync function!
+ * https://github.com/documentcloud/backbone/blob/master/backbone.js#L1424
+ */
+
+/*global Backbone:false, forge:false, _:false */
+
+(function() {
+  'use strict';
+
+  /*
+   * Since forge.request.ajax needs to use EmulatedHTTP, set it to true.
+   */
+  Backbone.emulateHTTP = true;
+
+  /*
+   * These are copied from the Backbone Library because they are declared as
+   * local functions, but we need them.
+   */
+
+  // Helper function to get a value from a Backbone object as a property
+  // or as a function.
+  var getValue = function(object, prop) {
+    if (!(object && object[prop])) return null;
+    return _.isFunction(object[prop]) ? object[prop]() : object[prop];
+  };
+
+  // Throw an error when a URL is needed, and none is supplied.
+  var urlError = function() {
+    throw new Error('A "url" property or function must be specified');
+  };
+
+  // Map from CRUD to HTTP for our default `Backbone.sync` implementation.
+  var methodMap = {
+    'create': 'POST',
+    'update': 'PUT',
+    'delete': 'DELETE',
+    'read':   'GET'
+  };
+
+  // Trigger.io Backbone.sync
+  // ------------------------
+  Backbone.sync = function(method, model, options) {
+    forge.logging.log('Backbone.sync.trigger.io ----');
+    var type = methodMap[method];
+
+    // Default options, unless specified.
+    options || (options = {});
+
+    // Default JSON-request options.
+    var params = {type: type, dataType: 'json'};
+
+    // Ensure that we have a URL.
+    if (!options.url) {
+      params.url = getValue(model, 'url') || urlError();
+    }
+
+    // Ensure that we have the appropriate request data.
+    if (!options.data && model && (method == 'create' || method == 'update')) {
+      params.contentType = 'application/json';
+      params.data = JSON.stringify(model.toJSON());
+    }
+
+    // For older servers, emulate JSON by encoding the request into an HTML-form.
+    if (Backbone.emulateJSON) {
+      params.contentType = 'application/x-www-form-urlencoded';
+      params.data = params.data ? {model: params.data} : {};
+    }
+
+    // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
+    // And an `X-HTTP-Method-Override` header.
+    if (Backbone.emulateHTTP) {
+      if (type === 'PUT' || type === 'DELETE') {
+        if (Backbone.emulateJSON) params.data._method = type;
+        params.type = 'POST';
+        params.beforeSend = function(xhr) {
+          xhr.setRequestHeader('X-HTTP-Method-Override', type);
+        };
+      }
+    }
+
+    // Don't process data on a non-GET request.
+    if (params.type !== 'GET' && !Backbone.emulateJSON) {
+      params.processData = false;
+    }
+
+    // Make the request, allowing the user to override any Ajax options.
+    forge.logging.log('Getting ready to make a request.');
+    return forge.request.ajax(_.extend(params, options));
+  };
+
+})();
